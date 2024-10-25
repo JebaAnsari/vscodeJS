ca4099536f0776c3be8d3fe639f1cd1df6d64bbd
Jeba Ansari <ansarijeba61@gmail.com>
2024-10-25 15:25:48 +0530

    object files

--- Js_classes_and_oops /object.js
+++ Js_classes_and_oops /object.js
@@ -5,3 +5,15 @@ function multipleBy5(num){
 console.log(multipleBy5(5));
 console.log(multipleBy5.power);
 console.log(multipleBy5.prototype);
+
+console.log('hii');
+function hello(hi){
+    return "jeba";
+}
+console.log(hello());
+function createUser(username,score){
+    this.username = username
+    this.score = score 
+
+}
+console.log(createUser("Jeba",8));
