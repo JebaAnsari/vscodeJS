697ba35733d17a2fdea6b392ccae5a3cadbca875
Jeba Ansari <ansarijeba61@gmail.com>
2024-10-28 13:19:08 +0530

    code to check rate limit

--- local_files/demo.js
+++ local_files/demo.js
@@ -1,1 +1,14 @@
-console.log('hii');
+// const rateLimitResponse = await fetch('https://api.github.com/rate_limit');
+// const rateLimitData = await rateLimitResponse.json();
+// console.log(rateLimitData); // Inspect rate limit data
+
+fetch('https://api.github.com/rate_limit')
+.then(function(response){
+    return response.json();
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>{
+    console.log('E:error',error);
+})
